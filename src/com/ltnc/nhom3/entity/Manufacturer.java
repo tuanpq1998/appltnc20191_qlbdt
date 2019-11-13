0f344d0f78ab4dd44356fcf6733e64211e1ad7b3
tuanpq1998 <tuanquang123321@gmail.com>
2019-11-13 20:50:00 +0700

    from bash with love

--- src/com/ltnc/nhom3/entity/Manufacturer.java
+++ src/com/ltnc/nhom3/entity/Manufacturer.java
@@ -10,41 +10,5 @@ package com.ltnc.nhom3.entity;
  * @author admin
  */
 public class Manufacturer {
-    private int id;
-    private String name;
-    private String country;
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public Manufacturer(int id, String name, String country) {
-        this.id = id;
-        this.name = name;
-        this.country = country;
-    }
-
-    public Manufacturer() {
-    }
-
+    
 }
