700a8314838391178b216b84d8525df9630d620d
tuanpq1998 <tuanquang123321@gmail.com>
2019-11-13 09:20:37 +0700

    test commit branch

--- src/com/ltnc/nhom3/dao/BillDao.java
+++ src/com/ltnc/nhom3/dao/BillDao.java
@@ -5,10 +5,45 @@
  */
 package com.ltnc.nhom3.dao;
 
+import com.ltnc.nhom3.entity.Bill;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  *
  * @author admin
  */
-public class BillDao {
+public class BillDao implements CrudDao<Bill> {
+
+    @Override
+    public boolean create(Bill t) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Bill> findAll() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Bill findById(int id) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(Bill t) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteById(int id) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Bill extractFromResultSet(ResultSet resultSet) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
