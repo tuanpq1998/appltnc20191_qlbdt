209a4b1d3c7460ed577de07c0925102b7226d752
phungsylinh <44540511+phungsylinh@users.noreply.github.com>
2019-12-12 21:08:51 +0700

    Add files via upload

---
+++ CustomerDao.java
@@ -0,0 +1,234 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ltnc.nhom3.dao;
+
+import com.ltnc.nhom3.connect.DatabaseConnect;
+import com.ltnc.nhom3.entity.Customer;
+import com.ltnc.nhom3.utility.DBQuery;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author admin
+ */
+public class CustomerDao {
+
+    public boolean create(Customer customer) throws SQLException {
+        int count = 0;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = DatabaseConnect.getInstance().getConnection();
+            preparedStatement = connection.prepareStatement(DBQuery.CREATE_NEW_CUSTOMER);
+
+            preparedStatement.setString(1, customer.getFullname());
+            preparedStatement.setString(2, customer.getAddress());
+            preparedStatement.setString(3, customer.getPhone());
+
+            count = preparedStatement.executeUpdate();
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        return count > 0;
+    }
+
+    public List<Customer> findAll(int offset, int limit) throws SQLException {
+        List<Customer> customers = null;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = DatabaseConnect.getInstance().getConnection();
+            preparedStatement = connection.prepareStatement(DBQuery.FIND_ALL_CUSTOMER_OFFSET_LIMIT);
+            preparedStatement.setInt(1, offset);
+            preparedStatement.setInt(2, limit);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            Customer customer = null;
+            customers = new ArrayList<>();
+
+            while (resultSet.next()) {
+                customer = extractFromResultSet(resultSet);
+                customers.add(customer);
+            }
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        return customers;
+    }
+
+    public Customer findById(int id) throws SQLException {
+        Customer customer = null;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = DatabaseConnect.getInstance().getConnection();
+            preparedStatement = connection.prepareStatement(DBQuery.FIND_CUSTOMER_BY_ID);
+
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                customer = extractFromResultSet(resultSet);
+
+                break;
+            }
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        return customer;
+    }
+
+    public boolean update(Customer customer) throws SQLException {
+        int count = 0;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = DatabaseConnect.getInstance().getConnection();
+            preparedStatement = connection.prepareStatement(DBQuery.UPDATE_CUSTOMER);
+
+            preparedStatement.setString(1, customer.getFullname());
+            preparedStatement.setString(2, customer.getAddress());
+            preparedStatement.setString(3, customer.getPhone());
+            preparedStatement.setInt(4, customer.getId());
+
+            count = preparedStatement.executeUpdate();
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        return count > 0;
+    }
+
+    public List<Customer> fillAllName(String name, int offset, int limit) throws SQLException {
+        List<Customer> customers = null;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = DatabaseConnect.getInstance().getConnection();
+            preparedStatement = connection.prepareStatement(DBQuery.FIND_ALL_CUSTOMER_BY_NAME);
+            preparedStatement.setString(1, "%" + name + "%");
+            preparedStatement.setInt(2, offset);
+            preparedStatement.setInt(3, limit);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            Customer product = null;
+            customers = new ArrayList<>();
+
+            while (resultSet.next()) {
+                product = extractFromResultSet(resultSet);
+                customers.add(product);
+            }
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        return customers;
+    }
+
+    public boolean deleteById(int id) throws SQLException {
+        int count = 0;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = DatabaseConnect.getInstance().getConnection();
+            preparedStatement = connection.prepareStatement(DBQuery.DELETE_CUSTOMER_BY_ID);
+
+            preparedStatement.setInt(1, id);
+
+            count = preparedStatement.executeUpdate();
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        return count > 0;
+    }
+
+    public int countAll() throws SQLException {
+        int count = 0;
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            connection = DatabaseConnect.getInstance().getConnection();
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(DBQuery.COUNT_ALL_CUSTOMER);
+            if (resultSet.next()) {
+                count = resultSet.getInt(1);
+            }
+        } finally {
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        return count;
+    }
+
+    public int countAllByName(String name) throws SQLException {
+        int count = 0;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = DatabaseConnect.getInstance().getConnection();
+            preparedStatement = connection.prepareStatement(DBQuery.COUNT_ALL_CUSTOMER_BY_NAME);
+            preparedStatement.setString(1, "%" + name + "%");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            Customer customer = null;
+            if (resultSet.next()) {
+                count = resultSet.getInt(1);
+            }
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        return count;
+    }
+
+    private Customer extractFromResultSet(ResultSet resultSet) throws SQLException {
+        Customer customer = new Customer();
+        customer.setId(resultSet.getInt(1));
+        customer.setFullname(resultSet.getString(2));
+        customer.setPhone(resultSet.getString(4));
+        customer.setAddress(resultSet.getString(3));
+        return customer;
+    }
+
+}
---
+++ CustomerService.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ltnc.nhom3.service;
+
+import com.ltnc.nhom3.dao.CustomerDao;
+import com.ltnc.nhom3.entity.Customer;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author PhungSyLinh
+ */
+public class CustomerService {
+
+    private CustomerDao customerDao;
+
+    public CustomerService() {
+        customerDao = new CustomerDao();
+    }
+
+    public List<Customer> findAll(int pageNum) throws SQLException {
+        return customerDao.findAll((pageNum-1) * 10,10);
+    }
+    public List<Customer> findByName(String searchKey,int pageNum)throws SQLException{
+        return customerDao.fillAllName(searchKey,(pageNum-1) * 10, 10);
+    }
+    public Customer findByID(int ID) throws SQLException {
+        return customerDao.findById(ID);
+    }
+
+    public boolean deleteByID(int idSelected) throws SQLException {
+        return customerDao.deleteById(idSelected);
+    }
+
+    public boolean create(Customer customer) throws SQLException {
+        return customerDao.create(customer);
+    }
+
+    public boolean update(Customer customer) throws SQLException {
+        return customerDao.update(customer);
+    }
+    public  int countAll()throws SQLException{
+        return customerDao.countAll();
+    }
+    public int countAllByName(String name) throws SQLException{
+        return customerDao.countAllByName(name);
+    }
+}
